8e2b1caec7af269c33f2503c18cf2dc5611e6e90
FALLENEZER <igumnov.danechka@mail.ru>
2024-08-03 20:42:30 +0300

    test

--- test.py
+++
@@ -1,9 +0,0 @@
-import telebot
-
-bot = telebot.TeleBot('7278409546:AAEzPe-WB5ePH9luglw_IhLHADZBlWpKgds')
-
-@bot.message_handler(commands=['start'])
-def main(message):
-    bot.send_message(message.chat.id, 'Добро пожаловать в наш магазин!')
-
-bot.polling(non_stop=True)
